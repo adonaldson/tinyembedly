af3ca543482665dcbe1628b8b0f7895f7f08944d
Andrew Donaldson <andrew@desto.net>
2012-01-12 13:45:06 +0000

    Move require httparty

--- lib/tinyembedly.rb
+++ lib/tinyembedly.rb
@@ -1,3 +1,4 @@
+require 'httparty'
 require "tinyembedly/version"
 require "tinyembedly/response_error"
 require "tinyembedly/oembed"
--- lib/tinyembedly/oembed.rb
+++ lib/tinyembedly/oembed.rb
@@ -1,5 +1,3 @@
-require 'httparty'
-
 module Tinyembedly
   class Oembed
     include HTTParty
